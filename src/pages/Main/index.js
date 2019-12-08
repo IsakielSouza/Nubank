ce43109e7d8632a7d679b66a1c5729d0319237d9
Isakiel Souza <isakielsouza@outlook.com.br>
2019-12-08 20:17:19 -0300

    Card Completed

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,15 +1,36 @@
 import React from 'react';
 
-import Header from '~/components/Header';
-import TabsItem from '~/components/Tabs';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { Container } from './styles';
+import Header from '~/components/Header';
+import Tabs from '~/components/Tabs';
+
+import { 
+  Container, Content, Card, CardHeader, CardContent, CardFooter, Title, Description, Annotation 
+} from './styles';
 
 export default function Main() {
   return(
     <Container>
       <Header />
-      <TabsItem />
+
+      <Content>
+        <Card>
+          <CardHeader>
+            <Icon name="attach-money" size={28} color="#666" />
+            <Icon name="visibility-off" size={28} color="#666" />
+          </CardHeader>
+          <CardContent>
+            <Title>Saldo disponível</Title>
+            <Description>R$ 1.201.665,01</Description>
+          </CardContent>
+          <CardFooter>
+            <Annotation>Transferencia de 20,00 recebida de Leticia Moreira hoje ás 08:00h</Annotation>
+          </CardFooter>
+        </Card>
+      </Content>
+
+      <Tabs />
     </Container>
   );
 }
